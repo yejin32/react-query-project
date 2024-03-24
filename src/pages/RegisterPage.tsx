93896ec01cf2c988814738c1d6802e7fe91066e5
yejin32 <vhdekddl3@naver.com>
2024-03-24 16:17:43 +0900

    Feat: RegisterPage에 ImgUploder InputField Button 적용

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+import ImgUploader from '../components/ImgUpload';
+import InputField from '../components/InputField';
+import Button from '../components/Button';
+
+const SRegisterContainer = styled.div``
 
 const RegisterPage = () => {
     return (
-        <div>
-            register
-        </div>
+        <SRegisterContainer>
+            <ImgUploader/>
+            <InputField id='email' type='email' placeholder='ID 를 입력해주세요'/>
+            <InputField id='password' type='password' placeholder='PW 를 입력해주세요'/>
+            <InputField id='passwordCheck' type='password' placeholder='PW 확인'/>
+            <InputField id='text' type='text' placeholder='이름을 입력해 주세요'/>
+            <Button id='registerBtn' text='회원가입'/>
+        </SRegisterContainer>
     );
 };
 

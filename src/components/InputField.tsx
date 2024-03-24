8989e40d9da8cf116bac00527a05d5390bc48af4
yejin32 <vhdekddl3@naver.com>
2024-03-24 17:02:19 +0900

    Design: InputField css 구현

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const SInputContainer= styled.div``;
+const SInputContainer= styled.div`
+    width: 60%;
+    display: flex;
+    justify-content: center;
+    
+    input {
+        text-align: center;
+        width: 100%;
+        height: 40px;
+        margin-bottom: 40px;
+        font-weight: 800;
+        font-size: 17px;
+        color: rgb(255, 255, 255);
+        background-color: rgb(47, 151, 254);
+        border: none;
+        border-radius: 2px;
+        outline: none;
+    }
+    input:focus::placeholder {
+            color: transparent;
+            
+        }
+    input::placeholder {
+            color: rgb(255, 255, 255);
+    }
+`;
 
 interface InputProps {
     id: string
@@ -12,7 +37,7 @@ interface InputProps {
 const InputField: React.FC<InputProps> = ({id, type, placeholder}: InputProps) => {
     return (
         <SInputContainer>
-            <input id={id} type={type} placeholder={placeholder}/> 
+            <input id={id} type={type} placeholder={placeholder} autoComplete='off'/> 
         </SInputContainer>
     );
 };

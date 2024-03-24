70c23e782d9496adf9b749d0e14d211db6a6cd48
yejin32 <vhdekddl3@naver.com>
2024-03-24 17:56:07 +0900

    Design: ImgUpload css 구현

--- src/components/ImgUpload.tsx
+++ src/components/ImgUpload.tsx
@@ -3,10 +3,35 @@ import styled from 'styled-components';
 
 
 const SImgUploadContainer = styled.div`
-`
-const SImgContainer = styled.div``;
+    width: 60%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 60px;
+    text-align: center;
+    color: rgb(255, 255, 255);
+    font-weight: 800;
+    font-size: 17px;
+    `
+const SImgContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: rgb(47, 151, 254); 
+    width: 85%;
+    height: 300px;
+    margin-bottom: 40px;
+    border-radius: 3px;
+`;
 
 const SImgBtnContainer = styled.div`
+    background-color: rgb(10, 90, 254);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 40px;
+    border-radius: 3px;
     input {
         display: none;
     }
@@ -28,8 +53,8 @@ const ImgUpload = () => {
         <SImgUploadContainer>
             <SImgContainer>
                 {fileName ? 
-                (<>선택된 파일: {fileName}</>) 
-                : (<>이미지를 첨부해 주세요.</>)}
+                (<div>선택된 파일: {fileName}</div>) 
+                : (<div>이미지를 첨부해 주세요</div>)}
             </SImgContainer>
             <SImgBtnContainer>
                 <label htmlFor='img'>이미지 업로드</label>

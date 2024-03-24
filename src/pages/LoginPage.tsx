63514f146d61cba1d9b0feb3f8b39ddeb5848550
yejin32 <vhdekddl3@naver.com>
2024-03-24 14:06:59 +0900

    Feat: LoginPage에 InputField Button 적용

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+import InputField from '../components/InputField';
+import Button from '../components/Button';
 
-const LoginPage = () => {
-    return (
-        <div>
-            login
-        </div>
+const SLoginContainer = styled.div`    
+`;
+
+const SBtnContainer = styled.div`
+    `;
+
+const LoginPage: React.FC = () => {
+    
+    return ( 
+        <SLoginContainer>
+            <InputField id='email' type='email' placeholder='ID 를 입력하세요'/>
+            <InputField id="password" type='password' placeholder='PW 를 입력해주세요'/>
+                <SBtnContainer>
+                    <Button id='registerBtn' text='회원가입'/>
+                    <Button id="loginBtn" text='로그인'/>
+                </SBtnContainer>
+        </SLoginContainer>
     );
 };
 

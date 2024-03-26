f345fdc8cee472d9013fb08d1f3d315c6f38fea9
yejin32 <vhdekddl3@naver.com>
2024-03-26 15:54:43 +0900

    Feat: LoginPage 레이아웃 구현

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -4,9 +4,17 @@ import InputField from '../components/InputField';
 import Button from '../components/Button';
 
 const SLoginContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
 `;
 
 const SBtnContainer = styled.div`
+    width: 60%;
+    display: flex;
+    justify-content: space-between;
 `;
 
 const LoginPage: React.FC = () => {
